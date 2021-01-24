719126937ad2bb48da6d180b8ba6c75b34df390a
Sandeep <sk9958814616@gmail.com>
2021-01-24 23:32:20 +0530

    adding screenshot

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -45,9 +45,9 @@ class Withdraw2000 implements WithdrawChain{
 			}
 		
 			//pass remainig amount to next note
-			if(remainder != 0){
-				this.chain.withdraw(new Amount(remainder),notes,counter);
-			}
+		
+			this.chain.withdraw(new Amount(remainder),notes,counter);
+			
 			
 		}else{
 			this.chain.withdraw(amt,notes,counter);
@@ -81,9 +81,9 @@ class Withdraw500 implements WithdrawChain{
 			}
 		
 			//pass remainig amount to next note
-			if(remainder != 0){
-				this.chain.withdraw(new Amount(remainder),notes,counter);
-			}
+		
+			this.chain.withdraw(new Amount(remainder),notes,counter);
+			
 			
 		}else{
 			this.chain.withdraw(amt,notes,counter);
@@ -116,9 +116,9 @@ class Withdraw200 implements WithdrawChain{
 			}
 		
 			//pass remainig amount to next note
-			if(remainder != 0){
-				this.chain.withdraw(new Amount(remainder),notes,counter);
-			}
+			
+			this.chain.withdraw(new Amount(remainder),notes,counter);
+			
 			
 		}else{
 			this.chain.withdraw(amt,notes,counter);
@@ -151,26 +151,39 @@ class Withdraw100 implements WithdrawChain{
 		}
 		
 	
-		//pass remainig amount to next note
+		//pass remaining amount to next note
 		if(remainder != 0){
-			this.chain.withdraw(new Amount(remainder),notes,counter);
+			if(notes.get(2000)!=0){
+				System.out.println("Enter amount in multiple of 2000");
+			}
+			if(notes.get(500)!=0){
+				System.out.println("Enter amount in multiple of 500");
+			}
+			if(notes.get(200)!=0){
+				System.out.println("Enter amount in multiple of 200");
+			}
+			if(notes.get(100)!=0){
+				System.out.println("Enter amount in multiple of 100");
+			}			
 		}else{
 			//withdrawing
 			if(counter.get(2000)>0)
-				System.out.println("Dispensing "+notes.get(2000)+" notes of 2000");
+				System.out.println("Dispensing "+counter.get(2000)+" notes of 2000");
 			if(counter.get(500)>0)
-				System.out.println("Dispensing "+notes.get(500)+" notes of 500");
+				System.out.println("Dispensing "+counter.get(500)+" notes of 500");
 			if(counter.get(200)>0)
-				System.out.println("Dispensing "+notes.get(200)+" notes of 200");
+				System.out.println("Dispensing "+counter.get(200)+" notes of 200");
 			if(counter.get(100)>0)
-				System.out.println("Dispensing "+notes.get(100)+" notes of 100");
+				System.out.println("Dispensing "+counter.get(100)+" notes of 100");
 			
+			//change in number of notes in the ATM
 			notes.put(2000,notes.get(2000)-counter.get(2000));
 			notes.put(500,notes.get(500)-counter.get(500));
 			notes.put(200,notes.get(200)-counter.get(200));
 			notes.put(100,notes.get(100)-counter.get(100));
 			
 		}
+		return;
 			
 	
 	}
@@ -246,10 +259,7 @@ class ATM{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		HashMap<Integer,Integer> counter = new HashMap<>();
-		counter.put(2000,0);
-		counter.put(500,0);
-		counter.put(200,0);
-		counter.put(100,0);
+		
 		ATM atm = new ATM();
 		//window
 		while(true){
@@ -272,8 +282,12 @@ class ATM{
 							System.out.println("Amount should be in multiple of 100");
 							
 						}else{
+							counter.put(2000,0);
+							counter.put(500,0);
+							counter.put(200,0);
+							counter.put(100,0);
 							atm.chain1.withdraw(new Amount(withAmount),atm.getNotes(),counter);
-							System.out.println("Total amount = "+atm.getTotalAmount());
+							//System.out.println("Total amount = "+atm.getTotalAmount());
 						}
 					}
 					
---
+++ ATM/README.md
@@ -0,0 +1,6 @@
+# Strike the max camps by the bombs
+
+## Output
+
+![output](https://github.com/Coderode/Problem-Solving/blob/master/ATM/pic1.JPG)
+![output](https://github.com/Coderode/Problem-Solving/blob/master/ATM/pic2.JPG)
